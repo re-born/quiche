8c20d232be0efdb4f8f30bd3e9dd83a286902b00
sosuke-k <snoopies.drum@gmail.com>
2014-05-19 15:57:41 -0700

    add tags to Item

--- rails/db/schema.rb
+++ rails/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140517235730) do
+ActiveRecord::Schema.define(version: 20140519224915) do
 
   create_table "comments", force: true do |t|
     t.text     "content"
@@ -31,10 +31,10 @@ ActiveRecord::Schema.define(version: 20140517235730) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text     "url"
-    t.binary   "screen_shot",     limit: 16777215
+    t.binary   "screen_shot",     limit: 921600
   end
 
-  add_index "items", ["user_id"], name: "index_items_on_user_id", using: :btree
+  add_index "items", ["user_id"], name: "index_items_on_user_id"
 
   create_table "readers", force: true do |t|
     t.integer  "user_id"
@@ -42,6 +42,25 @@ ActiveRecord::Schema.define(version: 20140517235730) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "taggings", force: true do |t|
+    t.integer  "tag_id"
+    t.integer  "taggable_id"
+    t.string   "taggable_type"
+    t.integer  "tagger_id"
+    t.string   "tagger_type"
+    t.string   "context",       limit: 128
+    t.datetime "created_at"
+  end
+
+  add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
+
+  create_table "tags", force: true do |t|
+    t.string  "name"
+    t.integer "taggings_count", default: 0
+  end
+
+  add_index "tags", ["name"], name: "index_tags_on_name", unique: true
 
   create_table "users", force: true do |t|
     t.string   "last_name"
