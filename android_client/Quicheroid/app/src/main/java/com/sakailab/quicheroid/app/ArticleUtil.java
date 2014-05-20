4618999317e26a6ebb91dcdb89365e27aada9de8
Hiroki Sakamoto <taisho6339@Hiroki-no-MacBook-Pro.local>
2014-05-21 01:44:49 +0900

    add save user id

--- android_client/Quicheroid/app/src/main/java/com/sakailab/quicheroid/app/ArticleUtil.java
+++ android_client/Quicheroid/app/src/main/java/com/sakailab/quicheroid/app/ArticleUtil.java
@@ -5,18 +5,24 @@ package com.sakailab.quicheroid.app;
  */
 public class ArticleUtil {
 
-    private final static String HTTP_LABEL = "http";
+    private final static String HTTP_LABEL = "http://";
+    private final static String HTTPS_LABEL = "https://";
 
     private ArticleUtil() {
     }
 
+    public static boolean isHttpUrl(String text) {
+        return text.contains(HTTP_LABEL);
+    }
+
+    public static boolean isHttpsUrl(String text) {
+        return text.contains(HTTPS_LABEL);
+    }
 
     public static String getURLString(String text) {
-        boolean isHttp = text.startsWith(HTTP_LABEL);
-        if (!isHttp)
-            return "";
-
         int index = text.indexOf(HTTP_LABEL);
+        if (index < 0)
+            index = text.indexOf(HTTPS_LABEL);
         return text.substring(index);
     }
 
--- android_client/Quicheroid/app/src/main/java/com/sakailab/quicheroid/app/MainActivity.java
+++ android_client/Quicheroid/app/src/main/java/com/sakailab/quicheroid/app/MainActivity.java
@@ -1,21 +1,45 @@
 package com.sakailab.quicheroid.app;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.webkit.URLUtil;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
+
+    private EditText mIDBox;
+    private Button mButton;
+    private SharedPreferences mPref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        boolean flag = URLUtil.isValidUrl("こんにちは http://www.google.com");
-        Log.d(Config.DEBUG_TAG, "flag:" + flag);
-        Log.d(Config.DEBUG_TAG, URLUtil.guessUrl("こんにちは http://www.google.com"));
+        mIDBox = (EditText) findViewById(R.id.twitter_id);
+        mButton = (Button) findViewById(R.id.save_button);
+        mPref = getSharedPreferences(Config.APP_CONF, MODE_PRIVATE);
+
+        String idText = mPref.getString(Config.CONF_ID, "");
+        mIDBox.setText(idText);
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String text = mIDBox.getText().toString();
+                if (text.equals(""))
+                    return;
+                SharedPreferences.Editor edit = mPref.edit();
+                edit.putString(Config.CONF_ID, text);
+                edit.commit();
+                Toast.makeText(MainActivity.this, R.string.save_id_message, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
 }
