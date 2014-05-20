89cd221d6571bc717f6b9b33f0e6eae9124012b6
daichi <daipanchi@gmail.com>
2014-05-20 23:27:50 +0900

    Create prototype

---
+++ rails/app/controllers/reader_controller.rb
@@ -0,0 +1,19 @@
+class ReaderController < ApplicationController
+  before_action :set_item
+
+  def toggle_reader
+    if @item.readers.pluck(:user_id).include?(current_user.id)
+      @reader = @item.readers.find_by({user_id: current_user.id}).destroy
+      status = 'destroy'
+    else
+      @reader = Reader.create({user_id: current_user.id, item: @item})
+      status = 'create'
+    end
+    render json: { status: status, data: @reader, image_url: @reader.user.image_url }
+  end
+
+  private
+    def set_item
+      @item = Item.find(params[:item_id])
+    end
+end
