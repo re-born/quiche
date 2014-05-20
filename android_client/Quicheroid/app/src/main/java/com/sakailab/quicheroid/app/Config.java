b2b87e421ac4240072b77ed9bac0893bd4ccd7e5
Hiroki Sakamoto <taisho6339@Hiroki-no-MacBook-Pro.local>
2014-05-21 01:44:49 +0900

    add save user id

--- android_client/Quicheroid/app/src/main/java/com/sakailab/quicheroid/app/Config.java
+++ android_client/Quicheroid/app/src/main/java/com/sakailab/quicheroid/app/Config.java
@@ -3,15 +3,18 @@ package com.sakailab.quicheroid.app;
 /**
  * Created by taisho6339 on 2014/05/19.
  */
-public class Config {
+public final class Config {
 
     public static String DEBUG_TAG = "DEBUG_TAG";
+
+    public static String APP_CONF = "APP_CONF";
+    public static String CONF_ID = "CONF_ID";
+
     public static String POST_API = "http://q.l0o0l.co/item/create";
+    public static String POST_PARAM_URL = "url";
+    public static String POST_PARAM_TITLE = "title";
+    public static String POST_PARAM_USER = "user";
+    public static String POST_PARAM_USER_OAUTH_TOKEN = "quiche_oauth_token";
+    public static String POST_PARAM_USER_TWITTER_ID = "quiche_twitter_id";
 
-    public static String POST_PARAM_URL ="url";
-    public static String POST_PARAM_TITLE ="title";
-    public static String POST_PARAM_USER ="user";
-    public static String POST_PARAM_USER_OAUTH_TOKEN ="quiche_oauth_token";
-    public static String POST_PARAM_USER_TWITTER_ID ="quiche_twitter_id";
-
-    }
+}
--- android_client/Quicheroid/app/src/main/java/com/sakailab/quicheroid/app/ReceiveActivity.java
+++ android_client/Quicheroid/app/src/main/java/com/sakailab/quicheroid/app/ReceiveActivity.java
@@ -2,6 +2,7 @@ package com.sakailab.quicheroid.app;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Window;
@@ -18,6 +19,8 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.Set;
+
 
 public class ReceiveActivity extends Activity {
 
@@ -30,11 +33,7 @@ public class ReceiveActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_receive_layout);
         setWindowInfo();
-
-        Log.d(Config.DEBUG_TAG, getIntent().getExtras().getString(Intent.EXTRA_TEXT));
-        Log.d(Config.DEBUG_TAG, ArticleUtil.getURLString(
-                ArticleUtil.getURLString(getIntent().getExtras().getString(Intent.EXTRA_TEXT))));
-        Log.d(Config.DEBUG_TAG, getIntent().getExtras().getString(Intent.EXTRA_SUBJECT));
+        mRequestQueue = Volley.newRequestQueue(this);
     }
 
     private void setWindowInfo() {
@@ -43,23 +42,57 @@ public class ReceiveActivity extends Activity {
         window.addFlags(WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE);
     }
 
+    private void validateArgument(Bundle extra) throws IllegalArgumentException {
+        Set<String> keySet = extra.keySet();
+        String text = extra.getString(Intent.EXTRA_TEXT);
+        if (!(keySet.contains(Intent.EXTRA_TEXT) && keySet.contains(Intent.EXTRA_SUBJECT)))
+            throw new IllegalArgumentException("ブラウザアプリ以外からの受信");
+
+        if (!(ArticleUtil.isHttpsUrl(text) || ArticleUtil.isHttpUrl(text)))
+            throw new IllegalArgumentException(text);
+
+        Log.d(Config.DEBUG_TAG, getIntent().getExtras().getString(Intent.EXTRA_TEXT));
+        Log.d(Config.DEBUG_TAG, ArticleUtil.getURLString(
+                ArticleUtil.getURLString(getIntent().getExtras().getString(Intent.EXTRA_TEXT))));
+        Log.d(Config.DEBUG_TAG, getIntent().getExtras().getString(Intent.EXTRA_SUBJECT));
+    }
+
     private void setPostRequest() {
         Bundle extra = getIntent().getExtras();
-        mRequestQueue = Volley.newRequestQueue(this);
-
         JSONObject reqParam = null;
+        SharedPreferences pref = getSharedPreferences(Config.APP_CONF, MODE_PRIVATE);
         try {
+            validateArgument(extra);
+
             reqParam = new JSONObject();
             String url = ArticleUtil.getURLString(extra.getString(Intent.EXTRA_TEXT));
-            if (url.equals(""))
-                throw new IllegalArgumentException(extra.getString(Intent.EXTRA_TEXT));
-
             reqParam.put(Config.POST_PARAM_TITLE, extra.getString(Intent.EXTRA_SUBJECT));
             reqParam.put(Config.POST_PARAM_URL, url);
             JSONObject userData = new JSONObject();
-            userData.put(Config.POST_PARAM_USER_TWITTER_ID, "taisho6339");
+            userData.put(Config.POST_PARAM_USER_TWITTER_ID, pref.getString(Config.CONF_ID, ""));
             userData.put(Config.POST_PARAM_USER_OAUTH_TOKEN, "aa1199u398tuoailajflaho");
             reqParam.put(Config.POST_PARAM_USER, userData);
+
+            mPostRequest = new JsonObjectRequest(Request.Method.POST, Config.POST_API, reqParam,
+                    new Response.Listener<JSONObject>() {
+                        @Override
+                        public void onResponse(JSONObject response) {
+                            Log.d(Config.DEBUG_TAG, response.toString());
+                            pushToast(R.string.post_article_success);
+                            finish();
+                        }
+                    },
+                    new Response.ErrorListener() {
+                        @Override
+                        public void onErrorResponse(VolleyError error) {
+                            error.printStackTrace();
+                            pushToast(R.string.post_article_success);
+                            finish();
+                        }
+                    }
+            );
+            mPostRequest.setTag(REQUEST_TAG);
+            mRequestQueue.add(mPostRequest);
 
         } catch (JSONException e) {
             e.printStackTrace();
@@ -68,31 +101,21 @@ public class ReceiveActivity extends Activity {
             Log.e(Config.DEBUG_TAG, "不正なURLを受け取りました.");
         }
 
-        mPostRequest = new JsonObjectRequest(Request.Method.POST, Config.POST_API, reqParam,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        Log.d(Config.DEBUG_TAG, response.toString());
-                    }
-                },
-                new Response.ErrorListener() {
-                    @Override
-                    public void onErrorResponse(VolleyError error) {
-                        error.printStackTrace();
-                        finish();
-                    }
-                }
-        );
+    }
 
-        mPostRequest.setTag(REQUEST_TAG);
-        mRequestQueue.add(mPostRequest);
+    private void pushToast(int textID) {
+        Toast.makeText(this, textID, Toast.LENGTH_LONG).show();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Toast.makeText(this, R.string.post_article_success, Toast.LENGTH_LONG).show();
-        finish();
+        setPostRequest();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
         if (mRequestQueue == null)
             return;
         mRequestQueue.start();
