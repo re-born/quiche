47e0c0cc098e9cb6db9961e102a37f8c32876862
daichi <daipanchi@gmail.com>
2014-06-07 23:50:20 +0900

    Enable API to return tags

--- rails/config/application.rb
+++ rails/config/application.rb
@@ -13,6 +13,8 @@ module LabCache
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.paths.add "app/api", glob: "**/*.rb"
+    config.autoload_paths += Dir["#{Rails.root}/app/api/*"]
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
