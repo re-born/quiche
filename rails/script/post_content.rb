784959781122db56cb1390e14545244f7e27233b
sosuke-k <snoopies.drum@gmail.com>
2014-06-13 22:49:09 -0700

    add sanitize to csv script

--- rails/script/post_content.rb
+++ rails/script/post_content.rb
@@ -1,4 +1,3 @@
-require 'fileutils'
 
 CONTENT_FILE_NAME = 'script/data_content.csv'
 TAG_FILE_NAME = 'script/data_tag.csv'
@@ -9,7 +8,9 @@ def write_content_and_tag item
   File.open(CONTENT_FILE_NAME, 'a') do |file|
     content = item.content
     TAGS_TO_BE_REMOVED.each { |pattern| content.gsub!(pattern, '') }
+    ActionController::Base.helpers.sanitize(content, tags: %w(div p))
     content.gsub!(' ', '')
+    content.gsub!('&#13', '')
     file.puts content
   end
   File.open(TAG_FILE_NAME, 'a') do |file|
