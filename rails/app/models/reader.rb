35dba2faf65f866276fe4f8209e55fbc8891817c
dch <daipanchi@gmail.com>
2014-11-27 02:48:34 +0900

    Notify new reader

--- rails/app/models/reader.rb
+++ rails/app/models/reader.rb
@@ -1,5 +1,16 @@
 class Reader < ActiveRecord::Base
+  require 'lib'
+  include Notification
+
   validates :user, uniqueness: {scope: :item}
   belongs_to :user
   belongs_to :item, touch: true
+
+  after_create :notify_new_reader
+
+  private
+
+  def notify_new_reader
+    slack_notify("@#{self.user.twitter_id} も 『#{self.item.title}』 を焼いたよ [##{self.item.id}](http://q.l0o0l.co/#item_#{self.item.id})", '#oven_gouter')
+  end
 end
