5105e3f938b589f140a0cdcd287311934c56ede6
Satomi <satomi.iijima@furman.edu>
2015-06-14 13:10:48 +0900

    Make _includes directory configurable

---
+++ ios_client/baker/bake/SelectionViewController.m
@@ -0,0 +1,14 @@
+//
+//  SelectionViewController.m
+//  baker
+//
+//  Created by hiroberry on 4/1/15.
+//  Copyright (c) 2015 hiroberry. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <MobileCoreServices/MobileCoreServices.h>
+
+@interface SreViewController ()
+
+@end
---
+++ ios_client/baker/bake/ShareViewController.h
@@ -0,0 +1,15 @@
+//
+//  ShareViewController.h
+//  bake
+//
+//  Created by hiroberry on 3/30/15.
+//  Copyright (c) 2015 hiroberry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : SLComposeServiceViewController
+
+
+@end
